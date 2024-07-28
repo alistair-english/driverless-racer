9077f0c55163aa4881cc6c87fd13cc179538ae6d
Alistair English <alistair.english@protonmail.com>
2024-07-29 07:51:14 +1000

    add a camera

--- src/hardware/hardware_gazebo/launch/_bridge.launch.py
+++ src/hardware/hardware_gazebo/launch/_bridge.launch.py
@@ -7,6 +7,8 @@ def generate_launch_description():
     bridge_topics = [
         "/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock",
         "/hardware/sensors/livox_front/points@sensor_msgs/msg/PointCloud2[ignition.msgs.PointCloudPacked",
+        "/hardware/sensors/camera_front/image_raw@sensor_msgs/msg/Image[ignition.msgs.Image",
+        "/hardware/sensors/camera_front/camera_info@sensor_msgs/msg/CameraInfo[ignition.msgs.CameraInfo",
     ]
 
     with l.namespace("simulation"):
